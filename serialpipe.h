321413a4768391bb89db92dd95bfd7df6874ed42
Emil Fresk <emil.fresk@gmail.com>
2015-11-17 16:11:33 +0100

    Added license header to files

--- serialpipe.h
+++ serialpipe.h
@@ -1,3 +1,23 @@
+/****************************************************************************
+*
+* Copyright (C) 2015 Emil Fresk.
+* All rights reserved.
+*
+* This file is part of the SerialPipe library.
+*
+* GNU Lesser General Public License Usage
+* This file may be used under the terms of the GNU Lesser
+* General Public License version 3.0 as published by the Free Software
+* Foundation and appearing in the file LICENSE included in the
+* packaging of this file.  Please review the following information to
+* ensure the GNU Lesser General Public License version 3.0 requirements
+* will be met: http://www.gnu.org/licenses/lgpl-3.0.html.
+*
+* If you have questions regarding the use of this file, please contact
+* Emil Fresk at emil.fresk@gmail.com.
+*
+****************************************************************************/
+
 #ifndef _SERIALPIPE_H
 #define _SERIALPIPE_H
 
